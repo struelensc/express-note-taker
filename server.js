2bca24a4bedc717e2aad1fc18d26a13089dc71d3
Courtney Struelens <courtneystruelens@hotmail.com>
2022-05-05 20:35:53 -0700

    linked notes page

--- server.js
+++ server.js
@@ -7,10 +7,15 @@ const app = express();
 
 app.use(express.static("public"));
 
+// GET Route for notes page
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
 // GET Route for homepage
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
-);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
